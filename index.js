08112f3f929580c2366a3d908bb7c60c183efb5e
Min-Hsuan Hung <poxuan33@gmail.com>
2018-09-05 09:20:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,9 +21,8 @@ function keepGoing(array, changeValue, skipValue){
 function findBy(array, findFn){
   for (let i = 0; i < array.length; i++){
     if (findFn(array[i])) {
-      break
+      return array[i]
     }
-    else
     return null 
   }
 }
