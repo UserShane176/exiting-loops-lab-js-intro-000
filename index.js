0dcc018ba9f60d297f784b2d2d8442ec29b983f6
Min-Hsuan Hung <poxuan33@gmail.com>
2018-09-05 09:11:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function breakOut(array, changeValue, stopValue){
     if (array[i] === stopValue){
       break
     }
-     changeValue.push(array[i])
+     array[i] = changeValue
   }
-  return
+  return array 
 }
